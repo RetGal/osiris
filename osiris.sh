d88b27026e121e01122937b61e5597f9d11ed638
RetGal <galante@puzzle.ch>
2020-04-26 20:07:12 +0200

    Initial commit

---
+++ osiris.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+workingDir=/home/bot/maverage
+scriptName=maverage.py
+params=
+exclude=mamaster
+minFree=40960
+
+if [[ ${minFree} -gt 0 ]]; then
+    free=`free | awk 'NR == 2{print $3}'`
+    if [[ ${free} -lt ${minFree} ]]; then
+       echo terminating all ${scriptName} instances
+       killall ${scriptName} 2>/dev/null
+       sleep 2
+    fi
+fi
+
+cd ${workingDir}
+find -name "*.pid" -type f 2>/dev/null | while read file;
+do
+  read pid instance < ${file}
+  if [[ ${instance} != ${exclude} ]]; then
+      kill -0 ${pid} 2>/dev/null
+      if [[ $? -eq 1 ]]; then
+        echo resurrecting ${instance}
+        tmux has-session -t ${instance} 2>/dev/null
+        if [[ $? -eq 1 ]]; then
+          tmux new -d -s ${instance}
+        fi
+        sleep 2
+        tmux send-keys -t "$instance" C-z "$workingDir/$scriptName $instance $params" C-m
+      else
+        echo ${instance} is alive
+      fi
+  fi
+done
